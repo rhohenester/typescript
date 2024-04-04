3d3cb94f123891d61c0be326d2c1e24a74c4c020
rhohenester <raissa@DESKTOP-A401TGM>
2024-04-04 10:04:55 +0200

    Add num check

--- js/js-only.js
+++ js/js-only.js
@@ -3,7 +3,13 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
-  return num1 + num2;
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    //A plus sign before the variable converts it to number
+    return +num1 + +num2;
+  }
+ 
 }
 
 button.addEventListener("click", function() {
