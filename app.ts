36ab66bd8fd45efd3b4c7385becc9ba9a6ac2449
Raissa Hohenester <raissa.developer@gmail.com>
2024-04-04 21:31:24 +0200

    throw new Error('Incorrect input') (#2)
    Co-authored-by: rhohenester <raissa@DESKTOP-A401TGM>

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 function add(n1: number, n2: number) {
-  console.log(typeof number1);
+  if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    throw new Error('Incorrect input!');
+  }
   return n1 + n2;
 }
 
