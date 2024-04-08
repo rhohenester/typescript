7b8d879c16aebc27f2932cfdb514244318488b0c
rhohenester <raissa@DESKTOP-A401TGM>
2024-04-08 15:29:46 +0200

    Add a Tuple

--- examples/array.ts
+++ examples/array.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Raissa',
   age: 33,
-  hobbies: ['Lift weights', 'Annoy Sisters']
+  hobbies: ['Lift weights', 'Annoy Sisters'],
+  role: [2, 'co-author']
 };
 let favoriteActivities: string[];
 
@@ -10,8 +16,17 @@ let favoriteActivities: string[];
 //favoriteActivities = ['Sports'];
 //console.log(person.name);
 
+//Tuple
+person.role.push('admin'); // push is an exception that is accepted in TS  
+
+//This generates a error:
+//person.role[1] = 10;
+
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   //Simulate TS Error:
   //console.log(hobby.map());
 }
+
+
+
