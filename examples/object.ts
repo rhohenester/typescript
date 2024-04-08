359577afef12a816e34590346589c1ad7ba09446
rhohenester <raissa@DESKTOP-A401TGM>
2024-04-08 12:11:49 +0200

    Example of TS object

---
+++ examples/object.ts
@@ -0,0 +1,18 @@
+// TS' representation of an object type that helps TypeScript
+// understand the objects you are working with
+/* const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Tete',
+    age: 31
+} */
+
+
+// js code:
+const person = {
+    name: 'Tete',
+    age: 31
+}
+
+console.log(person.name);
